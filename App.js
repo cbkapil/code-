50e971932355d79b86999b8da6ec3aa777e0dded
cbkapil <kapilkumar@cognicsys.com>
2022-09-30 01:30:12 -0700

    first

---
+++ App.js
@@ -0,0 +1,74 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import QMF2011_A from './Src/SHELL_SHOP/QMF2011/QMF2011_A'
+import QMF2011_B from './Src/SHELL_SHOP/QMF2011/QMF2011_B'
+import QMF2011_C from './Src/SHELL_SHOP/QMF2011/QM2011_C'
+import QMF_2011_D from './Src/SHELL_SHOP/QMF2011/QMF_2011_D'
+import QMF2011_E from './Src/SHELL_SHOP/QMF2011/QMF2011_E'
+import QMF2011_L2TL3T from  './Src/SHELL_SHOP/QMF2011/QMF2011_L2TL3T'
+import QMF2011_L2TL3T_B from './Src/SHELL_SHOP/QMF2011/QMF2011_L2TL3T_B'
+import QMF2011_L2TL3T_C from './Src/SHELL_SHOP/QMF2011/QMF2011_L2TL3T_C'
+import QMF2011_L2TL3T_D from './Src/SHELL_SHOP/QMF2011/QMF2011_L2TL3T_D'
+import QMF2011_L2TL3T_E from './Src/SHELL_SHOP/QMF2011/QMF2011_L2TL3T_E'
+import QMF2011_LGSLSCN_A from './Src/SHELL_SHOP/QMF2011/QMF2011_LGSLSCN_A'
+import QMF2011_LGSLSCN_B from  './Src/SHELL_SHOP/QMF2011/QMF2011_LGLSCN_B'
+import QMF2011_LGSLSCN_C from  './Src/SHELL_SHOP/QMF2011/QMF2011_LGSLSCN_C'
+import QMF2011_LGSLSCN_D from './Src/SHELL_SHOP/QMF2011/QMF2011_LGSLSCN_D'
+import QMF2011_LGSLSCN_E from './Src/SHELL_SHOP/QMF2011/QMF2011_LGSLSCN_E'
+import QMF2011_LPC_A from './Src/SHELL_SHOP/QMF2011/QMF2011_LPC_A'
+import QMF2011_LPC_B from './Src/SHELL_SHOP/QMF2011/QMF2011_LPC_B'
+import QMF2011_LPC_C from './Src/SHELL_SHOP/QMF2011/QMF2011_LPC_C'
+import QMF2011_LPC_D from './Src/SHELL_SHOP/QMF2011/QMF2011_LPC_D'
+
+import STACKNAVIGATOR from './Src/stackNavigation/stackNavigator'
+
+const App = () => {
+  return (
+    <View>
+ 
+      {/* <QMF2011_A />  */}
+
+      {/* <QMF2011_B/> */}
+      
+      {/* <QMF2011_C/> */}
+
+  {/* <QMF_2011_D/> */}
+
+  {/* <QMF2011_E/> */}
+
+  {/* <QMF2011_L2TL3T/> */}
+
+
+  {/* <QMF2011_L2TL3T_B/> */}
+     {/* <QMF2011_L2TL3T_C/> */}
+
+     {/* <QMF2011_L2TL3T_D/> */}
+
+     {/* < QMF2011_L2TL3T_E/> */}
+
+     {/* <QMF2011_LGSLSCN_A/> */}
+
+
+     {/* <QMF2011_LGSLSCN_B/> */}
+     {/* <QMF2011_LGSLSCN_C/> */}
+
+     {/* <QMF2011_LGSLSCN_D/> */}
+
+     {/* <QMF2011_LGSLSCN_E/> */}
+
+     {/* <QMF2011_LPC_A/> */}
+     {/* <QMF2011_LPC_B/> */}
+
+     {/* < QMF2011_LPC_C/> */}
+
+     {/* <QMF2011_LPC_D /> */}
+
+<STACKNAVIGATOR/>
+
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ components/divide.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const divide = () => {
+  return (
+    <View style={{
+        height: 1,
+        backgroundColor: 'rgba(255, 255, 255 ,0.3)',
+        alignSelf: 'stretch'
+    }} />
+  )
+}
+
+export default divide
+
+const styles = StyleSheet.create({})
